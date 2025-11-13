8259558ce4253b3643a2fe3ff99ff14c5bbbe029
griffin309 <2029horsg14@moundsviewschools.org>
2025-11-12 19:31:44 -0600

    added drivetrain file

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+
+
+
+public class DriveTrain extends SubsystemBase {
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {}
+  public WPI_TalonSRX left_motor = new WPI_TalonSRX(0);
+  public WPI_TalonSRX right_motor = new WPI_TalonSRX(1);
+
+  
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
