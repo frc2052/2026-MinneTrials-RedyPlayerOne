8dc9195baa2fdc0adc9ecab6fe457a313fde38b0
griffin309 <2029horsg14@moundsviewschools.org>
2025-11-17 19:59:19 -0600

    Made ArcadeDrive, Drivetrain, and Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,12 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -21,6 +24,12 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+private final Joystick m_controller = new Joystick(0);
+private final DriveTrain m_driveTrain = new DriveTrain();
+public Command getArcadeDriveCommand() {
+   return new ArcadeDrive(
+    m_driveTrain, () -> -m_controller.getRawAxis(1), () -> -m_controller.getRawAxis(5));
+}
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -45,11 +54,15 @@ public class RobotContainer {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
-
+m_driveTrain.setDefaultCommand(getArcadeDriveCommand());
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
+
+
+
+
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,16 +8,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 
 
 
 public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
   public DriveTrain() {}
-  public WPI_TalonSRX left_motor = new WPI_TalonSRX(0);
-  public WPI_TalonSRX right_motor = new WPI_TalonSRX(1);
 
-  
+  private WPI_TalonSRX left_motor = new WPI_TalonSRX(0);
+  private WPI_TalonSRX right_motor = new WPI_TalonSRX(1);
+
+  private final DifferentialDrive m_DifferentialDrive= new DifferentialDrive(left_motor::set, right_motor::set);
+
+  public void arcadeDrive(double left_speed, double right_speed){
+    m_DifferentialDrive.arcadeDrive(left_speed,right_speed);
+  }
+
 
 
   @Override
