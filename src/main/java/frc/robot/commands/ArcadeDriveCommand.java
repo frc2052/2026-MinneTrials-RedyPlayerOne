9fdd4811da9ce2f375132f5fa367494b2adb3d37
AlexToledo94 <cleizyweizy@gmail.com>
2025-11-17 20:43:22 -0600

    I made a branch

--- src/main/java/frc/robot/commands/ArcadeDriveCommand.java
+++ src/main/java/frc/robot/commands/ArcadeDriveCommand.java
@@ -4,23 +4,13 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ArcadeDriveCommand extends Command {
+  /** Creates a new ArcadeDriveCommand. */
+  public ArcadeDriveCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
