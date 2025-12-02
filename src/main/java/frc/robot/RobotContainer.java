e7831584c487544c1d9c2ae3167ae13358613242
griffin309 <2029horsg14@moundsviewschools.org>
2025-12-01 20:24:09 -0600

    Build made an update to their roller so i just updated the code. also updated the device numbers (everything should work)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -74,18 +74,21 @@ m_driveTrain.setDefaultCommand(getArcadeDriveCommand());
         .onTrue(new InstantCommand(() -> m_Shooter.shoot(), m_Shooter))
         .onFalse(new InstantCommand(() -> m_Shooter.shoot(), m_Shooter));
 
-double roller_speed = 0.1;
-double winch_speed = 0.1;
-        
+double roller_speed_top = 0.1;
+double roller_speed_bottom = -0.1;
+
+      
     JoystickButton joystickRollerIn = new JoystickButton(m_controller, 3);
     joystickRollerIn
-        .onTrue(new InstantCommand(() -> m_Intake.roller(0.0), m_Intake))
-        .onFalse(new InstantCommand(() -> m_Intake.roller(0.0), m_Intake));
+        .onTrue(new InstantCommand(() -> m_Intake.roller(roller_speed_top, roller_speed_bottom), m_Intake))
+        .onFalse(new InstantCommand(() -> m_Intake.roller(0.0, 0.0), m_Intake));
  
         JoystickButton joystickRollerOut = new JoystickButton(m_controller, 5);
         joystickRollerOut
-            .onTrue(new InstantCommand(() -> m_Intake.roller(0.0), m_Intake))
-            .onFalse(new InstantCommand(() -> m_Intake.roller(0.0), m_Intake));
+            .onTrue(new InstantCommand(() -> m_Intake.roller(-roller_speed_top, -roller_speed_bottom), m_Intake))
+            .onFalse(new InstantCommand(() -> m_Intake.roller(0.0, 0.0), m_Intake));
+
+            double winch_speed = 0.1;
 
 
             JoystickButton joystickWinchUp = new JoystickButton(m_controller,6);
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,11 +10,15 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Intake extends SubsystemBase {
     
-    private WPI_TalonSRX roller_motor = new WPI_TalonSRX(2);
+    private WPI_TalonSRX roller_motor_top = new WPI_TalonSRX(6);
+    private WPI_TalonSRX roller_motor_bottom = new WPI_TalonSRX(2);
     private WPI_TalonSRX winch_motor = new WPI_TalonSRX(3);
 
-public void roller(double roller_speed){
-roller_motor.set(roller_speed);
+public void roller( double roller_motor_speed_top, double roller_motor_speed_bottom){
+roller_motor_top.set(roller_motor_speed_top);
+roller_motor_bottom.set(roller_motor_speed_bottom);
+
+
 }
 public void winch(double winch_speed){
 
