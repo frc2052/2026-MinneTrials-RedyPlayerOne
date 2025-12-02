f34d081093dfefcff7a8165fc346ef21c0206d07
Noah_lindquist <2029lindn69@moundsviewschools.org>
2025-12-01 20:24:19 -0600

    wait i forgot the device number oops

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,8 +11,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
   public Intake() {}
-  private WPI_TalonSRX roller_motor_top = new WPI_TalonSRX(0);
-  private WPI_TalonSRX roller_motor_bottom = new WPI_TalonSRX(0);
+  private WPI_TalonSRX roller_motor_top = new WPI_TalonSRX(6);
+  private WPI_TalonSRX roller_motor_bottom = new WPI_TalonSRX(2);
   private WPI_TalonSRX winch_motor = new WPI_TalonSRX(3);
 
 
