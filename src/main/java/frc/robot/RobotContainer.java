ab4702d325b3678a1d62e7623cdc8300ca45b3e2
Noah_lindquist <2029lindn69@moundsviewschools.org>
2025-12-08 19:37:08 -0600

    made the auto code work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,8 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
@@ -50,6 +52,13 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return null;
+    
+    return new SequentialCommandGroup(
+
+       new ParallelCommandGroup(
+          new ArcadeDriveCommand(drivetrain, () -> 1.0, () -> 0.0),
+         new WinchUpCommand(intake)).withTimeout(3),
+
+    new ShooterCommand(shooter));
   }
 }
